62cafdb5fa36c0601554561bba65561a2a183de5
AndrewR <andrew.rands@bluestone.co.uk>
2020-08-05 18:48:27 +0100

    initial commit

---
+++ PublishSubscribe.Web/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PublishSubscribe.Web;
+
+namespace ServiceBusFacade.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        //private readonly IMessagePublisher _messagePublisher;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            //_messagePublisher = messagePublisher;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+         //   _messagePublisher.Publish(new WeatherForecastsRequestedMessage());
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ PublishSubscribe.Web/Subscribers/WeatherForecastsRequestedMessageSubscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceBusFacade.Web.Subscribers
+{
+    public class Class
+    {
+    }
+}
